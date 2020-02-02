44839ea4066564aa7ca8efde31a7bbc0aa18221b
DeriqDanois <deriqdanois@gmail.com>
2020-02-01 19:53:07 -0800

    card near completion

--- src/comps/cards/index.js
+++ src/comps/cards/index.js
@@ -1,35 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
-function Card({isOn, }){
-    if (isOn === true) {
-        
+function Card({ }){
+    const [inbound, setInbound] = useState(1)
+    const [outbound, setOutBound] = useState(0)
+    const [active, setActive] = useState(true)
     
+// #1 Makes Tab color change on click
+    var cnOne = "tab1";
+    var cnTwo = "tab2";
+
+    if ( inbound === 1 ){
+        cnOne = "tab1 tab:active";
+    } else if ( inbound === 0 ) {
+        cnOne = "tab1 tab-inactive"
+    }
+
+    if ( outbound === 1 ){
+        cnTwo = "tab2 tab:active";
+    } else if ( outbound === 0 ) {
+        cnTwo = "tab2 tab-inactive"
+    }
+//  end
+
     return (
 
         <div className="card">
             <div className="tab-container">
        
-                <div className="tab1">INBOUND</div>
-                <div className="tab2">OUTBOUND</div>
+        
+            <div 
+                className={cnOne} 
+                onClick={()=>{
+                setInbound(1)
+                setOutBound(0)
+                setActive(active)
+             }}>INBOUND
+             </div>
+            <div 
+                className={cnTwo} 
+                onClick={()=>{
+                setInbound(0)
+                setOutBound(1)
+             }}>OUTBOUND
+             </div>
                 <div className="white-space"></div>
-              
             </div>
         
             <div className="body">
-          
-                item-holder card
+                Filter goes here
             </div>
+
         </div>
           )
 
 
 }
-}
+
 Card.defaultProps = {
-    className:"card",
-    isOn: true
+ 
 }
 
 
