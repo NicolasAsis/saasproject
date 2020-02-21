26185b90ace95e6b589a8456eb364fa992f7b498
NicolasAsis <nico.asis15@gmail.com>
2020-02-20 16:30:56 -0800

    Finished 'button' comp

--- src/comps/mainButton/index.js
+++ src/comps/mainButton/index.js
@@ -6,22 +6,39 @@ import {FiMoreHorizontal} from 'react-icons/fi';
 
 import { IoIosClose } from "react-icons/io";
 
+import * as FeatherIcon from 'react-icons/fi';
 
-function MainButton({defaultText}){
+
+
+
+function MainButton({defaultText,width,height,textSize,addIcon,customIcon}){
 
     const [close,setClose] = useState(false);
+
+    var Icon = FeatherIcon[customIcon];
    
     return (
-        <div className="butCont">
-                <p className="butText">{defaultText}</p>
+        <div 
+        className="butCont"
+        style={{
+            width:width,
+            height:height
+        }}
+        >
+                {addIcon&&<Icon color='white' size={15}/> }
+                <p className="butText" style={{fontSize:textSize}}>{defaultText}</p>
         </div>
     )
 }
 
 MainButton.defaultProps = {
-    defaultText:"Default Text"
+    defaultText:"Default Text",
+    width:"10%",
+    height:"39px",
+    textSize:"15px",
+    addIcon:false,
+    customIcon:"FiSave"
 }
-
 
 
 export default MainButton;
--- src/stories/mainButton.stories.js
+++ src/stories/mainButton.stories.js
@@ -11,3 +11,19 @@ export default {
 export const DefaultButton = () => {
     return <MainButton />
 }
+
+export const CustomDimensionsButton = () => {
+    return <MainButton 
+        width="30%"
+        height="39px"
+        textSize="15px"
+    />
+}
+
+export const ButtonWithIcon = () => {
+    return <MainButton 
+        addIcon="true"
+        height="39px"
+        textSize="15px"
+    />
+}
